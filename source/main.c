8280499f91c57d871037658cd3b4ac0303d64302
Cruzer-S <yyyynoom@gmail.com>
2024-06-23 03:59:22 -0400

    add example code

--- source/main.c
+++ source/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
+#include "net-util.h"
+
+#include <net/if.h>
+#include <netdb.h>
+
 int main(int argc, char *argv[])
 {
-	printf("hello, world!\n");
+	struct list *address = get_interface_address(
+		AF_INET6, IFF_UP, IFF_LOOPBACK
+	);
+
+	if (LIST_IS_EMPTY(address))
+		return 1;
+
+	printf("Hosts: ");
+	LIST_FOREACH_ENTRY(address, addr, struct address_data_node, list) {
+		char *host = get_host_from_address(
+			&addr->address, NI_NUMERICHOST
+		);
+
+		if (host == NULL) continue;
+
+		printf("%s\t", host);
+	} putchar('\n');
+
+	free_interface_address(address);
 
 	return 0;
 }
